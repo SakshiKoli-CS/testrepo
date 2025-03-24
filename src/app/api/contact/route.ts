7a5ecbeaa0b21bff620eb84de79498cbb1b57ee6
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-24 16:57:51 +0530

    fixed unused vars

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,8 @@
 
  export async function POST(request: Request) {
   const data = request.body;
-  let uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  console.log(data);
+  const  uid = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
   return new Response('Hello, Next.js!', {
     status: 200,
     headers: { 'Set-Cookie': `token=${uid}` },
