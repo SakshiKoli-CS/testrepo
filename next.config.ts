89e02107d8de2500dadb053b9869663c1ff4cf17
Aryan Bansal <aryan.bansal@contentstack.com>
2025-05-14 14:41:15 +0530

    modified config file to include cache headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/about", 
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, s-maxage=31536000, stale-while-revalidate=60"
+          }
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
